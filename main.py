2572f1065e8f464d315abad3f22b27ddf1ce959b
Михайлов Никита Викторович <nvmihajlov@mts.ru>
2025-11-08 21:01:22 +0300

    pre-release 0.3.4

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils.paths import pm
 import os
-os.makedirs(pm.get_executable_dir_path("attachments"), exist_ok=True)
+#os.makedirs(pm.get_executable_dir_path("attachments"), exist_ok=True)
 from ui.gui import App
 
 if __name__ == "__main__":
